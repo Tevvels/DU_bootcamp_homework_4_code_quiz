c6367a5343a495daeaf7f71ec002444304489ad2
Christopher Watkins <chrisbwatkins@gmail.com>
2021-01-24 15:49:55 -0700

    taking some notes about how the functino of the project will work

---
+++ assets/javascript/script.js
@@ -0,0 +1,64 @@
+
+
+// Code Quiz
+
+// var timer;
+// var question = [];
+// var correctAnwswer;
+
+
+
+
+// startQuiz = function(){
+
+    // creates the space where the questions will be asked.
+    // multiple choice questions need 4 buttons.
+    // runs responds and gives points or deducts time 
+
+    // display
+    // TimeR
+    // buttons
+    // question
+    // question staging 
+    // points.
+
+
+    // question cycle process 
+        // dynamic 
+        //     start button pushed
+
+
+            
+
+
+                // its a iteration that slowly counts down to zero
+                // correct answers are rewarded points * timer
+                    // uncorrect answers are deducted timer - x value 
+
+                
+
+
+        //         div with for options to choose 
+        //         probably a form with a radiobutton with the options.
+                // after the question is picked the value is stored. the last questions are .removed
+                // and  a new set with those value comes up 
+
+
+
+
+                
+                
+
+
+
+
+// };
+
+// endgame = function(){
+
+    // this runs after the questions or timer has completed
+    
+//     display
+//         list of highscores
+//         button for replay
+// }
