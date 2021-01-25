e6074a19ae901bf7d60174c96d9a1e064e4c429e
Christopher Watkins <chrisbwatkins@gmail.com>
2021-01-24 19:50:06 -0700

    made a question object and found my 4 choices through the DOm

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,29 @@
 
+// variables that connect to elements on the DOM 
+var screen = document.getElementById("questionScreen");
+var choices = document.getElementById("questionChoices");
+
+
+
+// variables needed for the script 
+var timer;
+var questions = ["question 1","question 2", "question 3", "question 4"];
+var answers;
+var correctAnwswer ;
+var randomQuestion; 
+var questionChoices;
+// variable to create elements 
+
+var button = document.createElement("button");
+
+button.addEventListener("click",function(){
+    startQuiz();
+});
+button.textContent = "click me";
+button.setAttribute("class","button");
+
+
+screen.appendChild(button);
 
 // Code Quiz
 
@@ -6,12 +31,54 @@
 // var question = [];
 // var correctAnwswer;
 
+startQuiz = function(){
+    buildQuestionaire = function(){
+       randomQuestion = Math.floor(Math.random() * questions.length);
+        screen.textContent = questions[randomQuestion];
+
+    
+        questionChoices = document.createElement("button");
+        questionChoices.setAttribute("class","questionChoices");
+
+
+        
+        choices.appendChild(questionChoices);
+   
+
+
+    }
+    choices.textContent = '';
+    buildQuestionaire();
+    buildQuestionaire();
+    buildQuestionaire();
+    buildQuestionaire();
+
+    var questionOne = {
+        question: "what kind of coding langauge is JavaScript",
+        answer: "Object oriented language",
+        wrongAnswers: ["a framework","Procedural Programming Language","Logic Programming Language"],
+    }
+
+    
+
+    
+    questionChoices.addEventListener("click",function(){
 
 
 
-// startQuiz = function(){
+    
+
+    });
+
+    document.body.children[1].children[1].children[0].children[2].style.background = "red";
+
+
+}
 
     // creates the space where the questions will be asked.
+
+
+
     // multiple choice questions need 4 buttons.
     // runs responds and gives points or deducts time 
 
@@ -35,7 +102,7 @@
                 // correct answers are rewarded points * timer
                     // uncorrect answers are deducted timer - x value 
 
-                
+
 
 
         //         div with for options to choose 
